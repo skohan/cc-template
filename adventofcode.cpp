8b7922840d179488c248170d5a9a7e37a6852738
sohan <sohan@email.com>
2024-05-23 13:18:13 +0530

    advent of code file

---
+++ adventofcode.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+#define nl "\n"
+
+const int N = 3 * 1e5 + 2;
+// const int mod = 1e9 + 7;
+
+// Alphabets ascii value
+int smalla = 97;
+int biga = 65;
+
+void solve() {
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    int c = 1;
+
+    while (t--)
+    {
+        // cout << "Case #" << c <<": " ;
+
+        solve();
+        c++;
+    }
+}
